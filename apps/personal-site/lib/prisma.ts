0305629e1952b0dda3f3a48df1c35fa1c93cfc91
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2022-02-12 13:45:04 -0500

    Complete build

--- apps/personal-site/lib/prisma.ts
+++ apps/personal-site/lib/prisma.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-interface CustomNodeJsGlobal extends NodeJS.Global {
-  prisma: PrismaClient;
+// https://stackoverflow.com/a/68328402
+declare global {
+  var prisma: PrismaClient; // eslint-disable-line
 }
 
 // Prevent multiple instances of Prisma Client in development
 // https://www.prisma.io/docs/guides/performance-and-optimization/connection-management#prevent-hot-reloading-from-creating-new-instances-of-prismaclient
 // https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient/instantiate-prisma-client#the-number-of-prismaclient-instances-matters
-declare const global: CustomNodeJsGlobal;
-
 const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
