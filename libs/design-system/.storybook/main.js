6e52709383a5a9b95b1b16e26625092185b99a00
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2022-02-12 13:30:27 -0500

    Upgrade storybook

--- libs/design-system/.storybook/main.js
+++ libs/design-system/.storybook/main.js
@@ -3,6 +3,8 @@ const rootMain = require('../../../.storybook/main');
 module.exports = {
   ...rootMain,
 
+  core: { ...rootMain.core, builder: 'webpack5' },
+
   stories: [
     ...rootMain.stories,
     '../src/lib/**/*.stories.mdx',
